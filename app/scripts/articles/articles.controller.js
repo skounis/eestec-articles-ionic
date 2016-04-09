13077effb132b5acdb5da00b4d5f526e90cd79a8
Stavros Kounis <skounis@gmail.com>
2016-04-09 09:15:47 +0300

    feat(articles) Fetch and display a list of articles

--- app/scripts/articles/articles.controller.js
+++ app/scripts/articles/articles.controller.js
@@ -5,16 +5,21 @@
 		.module('supermodular.articles')
 		.controller('ArticlesController', ArticlesController);
 
-	ArticlesController.$inject = ['$state'];
+	ArticlesController.$inject = ['$scope', '$state', 'articlesService'];
 
 	/* @ngInject */
-	function ArticlesController($state) {
+	function ArticlesController($scope, $state, articlesService) {
 
-    var vm = angular.extend(this, {
+		var vm = angular.extend(this, {
+			articles: [],
 			navigate: navigate
 		});
 
-    // ********************************************************************
+		// ********************************************************************
+
+		articlesService.all(function(data){
+			vm.articles = data;
+		});
 
     function navigate() {
       $state.go('app.article');
--- app/scripts/articles/articles.service.js
+++ app/scripts/articles/articles.service.js
@@ -5,10 +5,13 @@
 		.module('supermodular.articles')
 		.factory('articlesService', articlesService);
 
-	articlesService.$inject = [];
+	articlesService.$inject = ['$http', '$q'];
 
 	/* @ngInject */
-	function articlesService() {
+	function articlesService($http, $q) {
+
+		var url = 'http://skounis.s3.amazonaws.com/mobile-apps/barebone-glossy/news.json';
+		var result = [];
 
 		var service = {
 			all: all,
@@ -20,8 +23,19 @@
 
 		// http://stackoverflow.com/questions/17533888/s3-access-control-allow-origin-header
 		function all(callback){
-      var result = []
-      callback(result);
+			$http.get(url)
+				.success(function(data, status, headers, config) {
+					// this callback will be called asynchronously
+					// when the response is available
+					result = data.result;
+					callback(result);
+				})
+				.error(function(data, status, headers, config) {
+					// called asynchronously if an error occurs
+					// or server returns response with an error status.
+					console.log('ERROR (News):' + status);
+					callback(result);
+				});
 		}
 
 		function get(articleId) {
