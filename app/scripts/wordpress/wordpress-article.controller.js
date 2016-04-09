4a6742aa0fdf5849755b76cd2f31e70ea80deba4
Stavros Kounis <skounis@gmail.com>
2016-04-09 10:13:00 +0300

    feat(wp) Display a single article

--- app/scripts/wordpress/wordpress-article.controller.js
+++ app/scripts/wordpress/wordpress-article.controller.js
@@ -5,13 +5,29 @@
 		.module('supermodular.wordpress')
 		.controller('WordpressArticleController', WordpressArticleController);
 
-	WordpressArticleController.$inject = [];
+	WordpressArticleController.$inject = [
+		'$stateParams', 'wordpressService'];
 
 	/* @ngInject */
-	function WordpressArticleController() {
+	function WordpressArticleController($stateParams, wordpressService) {
+		var articleId = parseInt($stateParams.articleId, 10);
+
+		var vm = angular.extend(this, {
+			article: null,
+		});
+
+		function activate() {
+			loadArticle();
+		}
+		activate();
 
 		// ********************************************************************
 
-
+		function loadArticle() {
+			wordpressService.getArticle(articleId)
+				.then(function(article) {
+					vm.article = article;
+				});
+		}
 	}
 })();
--- app/scripts/wordpress/wordpress-articles.controller.js
+++ app/scripts/wordpress/wordpress-articles.controller.js
@@ -5,12 +5,31 @@
 		.module('supermodular.wordpress')
 		.controller('WordpressArticlesController', WordpressArticlesController);
 
-	WordpressArticlesController.$inject = [];
+	WordpressArticlesController.$inject = ['$state', 'wordpressService'];
 
 	/* @ngInject */
-	function WordpressArticlesController() {
+	function WordpressArticlesController($state, wordpressService) {
+		var vm = angular.extend(this, {
+			articles: [],
+			navigate: navigate
+		});
+
+		function activate() {
+			getArticles();
+		}
+		activate();
 
 		// ********************************************************************
 
+		function getArticles() {
+			wordpressService.getArticles()
+				.then(function(articles) {
+					vm.articles = articles;
+				});
+		}
+
+		function navigate(articleId) {
+			$state.go('app.wordpress-article', { articleId: articleId });
+		}
 	}
 })();
